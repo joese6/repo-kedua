f1ba9aca4284635b268747ed20612226d9f0671b
Johan <joese6@gmail.com>
2022-05-30 10:05:29 +0700

    feat: penambahan script penjumlahan dalam scope console.log

--- index.js
+++ index.js
@@ -1,2 +1,7 @@
 console.log(" Halo, aku repo kedua, dibuat oleh Zohan");
 console.log(" Halo, ini adalah tambahan console untuk repo-kedua");
+
+let n1=4;
+let n2=5;
+
+console.log(`Hasil penjumlahan 4 + 5 adalah : ${n1+n2}`);
